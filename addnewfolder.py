8fb865182bca80b63fbaa344a96a876a8a5311f2
danielswift89 <67100309+danielswift89@users.noreply.github.com>
2023-08-18 22:49:16 +0800

    Update addnewfolder.py

--- addnewfolder.py
+++ addnewfolder.py
@@ -16,8 +16,8 @@ def create_folders_from_file(path, file_path):
         print(f'已創建資料夾：{folder_path}')
 
 # 指定路徑和檔案路徑（請使用Unicode編碼表示中文）
-directory_path = r'C:\Users\daniel\OneDrive - 嘉南藥理大學\桌面\Pics'
-file_path = r'C:\Users\daniel\OneDrive - 嘉南藥理大學\桌面\Pics\課表.txt'
+directory_path = r'C:\Users\daniel\'
+file_path = r'C:\Users\daniel\yourfile.txt'
 
 # 呼叫函式以創建資料夾
 create_folders_from_file(directory_path, file_path)
